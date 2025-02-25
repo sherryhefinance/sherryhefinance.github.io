3df2bbcab462e71b4fdc3a6cbe13c79b9f78ac18
sherry9359 <xinman001@e.ntu.edu.sg>
2025-02-25 12:30:03 +0000

    Deploying to gh-pages from @ sherry9359/sherry9359.github.io@e43d6e2ec9ee9429a8101d5cbb5bc6f6e135bdf7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -431,12 +431,7 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2-copy/";
-            },},{id: "news-presentation-the-36th-asian-finance-association-annual-conference",
+    },{id: "news-presentation-the-36th-asian-finance-association-annual-conference",
           title: 'Presentation: The 36th Asian Finance Association Annual Conference',
           description: "",
           section: "News",},{id: "news-presentation-singapore-scholars-symposium-2024",
@@ -498,35 +493,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%78%69%6E%6D%61%6E%30%30%31@%65.%6E%74%75.%65%64%75.%73%67", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://www.linkedin.com/in/sherry-h-46a56b12b", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.douyin.com/user/MS4wLjABAAAA0ZkCt_W4UD9LmQa2HPtZp1YiozTzV4qcvRupWSsI3JU", "_blank");
         },
       },{
       id: 'light-theme',
